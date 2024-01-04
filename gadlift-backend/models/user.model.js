4b2916fc4f858da3e16211c4ce28522105754e7a
younes_damouna <younes.damouna@gmail.com>
2024-01-04 10:41:03 +0200

    created User Model

---
+++ gadlift-backend/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6,
+        trim: true
+    },
+    password: {
+        type: String,
+        requiredtrue,
+        minlength: 8
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+})
+userSchema.pre("save", async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+},
+    {
+        timestamps: true,
+    }
+)
+const User = mongoose.model("User", userSchema)
+module.exports = User
