58c0149d89ddcedc5e0cd4e73b6d9d03a96df55d
younes_damouna <younes.damouna@gmail.com>
2024-01-08 13:47:05 +0200

    backend: added data validation to UserSchema

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,25 +1,40 @@
-import {Prop,Schema,SchemaFactory} from '@nestjs/mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { HydratedDocument } from 'mongoose'
 
-export type UserDocument= HydratedDocument<User>;
+export type UserDocument = HydratedDocument<User>;
 @Schema()
-export class User{
-    @Prop()
-    first_name:string;
+export class User {
+    @Prop({
+        required: true,
+        minlength: 6
+    })
+    first_name: string;
 
-    @Prop()
-    last_name:string;
+    @Prop({
+        required: true,
+        minlength: 6
+    })
+    last_name: string;
 
-    @Prop()
-    email:string;
+    @Prop({
+        required: true,
+        unique: true,
+        minlength: 6,
+        trim: true
+    })
+    email: string;
 
-    @Prop()
-    password:string
+    @Prop({
+      
+        required: true,
+        minlength: 8
+    })
+    password: string;
 
-    @Prop()
-    user_type:string;
+    @Prop({ default:"User"})
+    user_type: string;
 
 
 }
 
-export const UserSchema=SchemaFactory.createForClass(User);
+export const UserSchema = SchemaFactory.createForClass(User);
