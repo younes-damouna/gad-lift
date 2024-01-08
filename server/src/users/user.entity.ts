13919d8db6f29d8750d7bc00a4f363f61accefee
younes_damouna <younes.damouna@gmail.com>
2024-01-08 20:44:04 +0200

    backend: created UserEntity

--- server/src/users/user.entity.ts
+++ server/src/users/user.entity.ts
@@ -1,7 +1,7 @@
-export class User{
-    first_name:string;
-    last_name:string;
-    email:string;
-    password:string;
-    user_type:string;
+export class User {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    user_type: string;
 }
