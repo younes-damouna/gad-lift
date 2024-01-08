eed7d559e890594b062199ed0bc37e61b54551ad
younes_damouna <younes.damouna@gmail.com>
2024-01-08 13:29:07 +0200

    backend: imported mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true})],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    // importing Mongoose model and load env url
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:async (config:ConfigService)=>({
+        uri:config.get<string>('MONGODB_URL')
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService=app.get(ConfigService);
-  const port=configService.get<number>('PORT')
+  const port=configService.get<number>('PORT');
 
   await app.listen(port);
 }
