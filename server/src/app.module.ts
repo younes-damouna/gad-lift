0082a068123fa41f2e9b9db6704414e160ab88d8
younes_damouna <younes.damouna@gmail.com>
2024-01-08 13:14:16 +0200

    backend: configured .env variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({isGlobal:true})],
   controllers: [AppController],
   providers: [AppService],
 })
