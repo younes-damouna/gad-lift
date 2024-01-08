be2822967bac54c6b51b8d14fbf442f6b7060a17
younes_damouna <younes.damouna@gmail.com>
2024-01-08 12:05:44 +0200

    backend: initialized NestJs

--- gadlift-backend/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const {login,register}=require("../controllers/auth.controllers")
-const router =express.Router()
-router.post("/login",login)
-router.post("/register",register)
-
-module.exports=router
