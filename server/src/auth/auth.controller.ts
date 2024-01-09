afe374f8acfbb79cf2ac1c2b4dc0e1873eea50ec
younes_damouna <younes.damouna@gmail.com>
2024-01-09 14:44:28 +0200

    backend: used localGuard in authController

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,12 +2,15 @@ import { Body, Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from
 import { AuthService } from './auth.service';
 import { User } from 'src/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService:AuthService){}
     @HttpCode(HttpStatus.OK)
-    @UseGuards(AuthGuard('local'))
+    // @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
+
     @Post('login')
     login(@Request() req:User){
         // console.log(req)
