56694eebcd37f65404dd378730b852a28f7ed400
younes_damouna <younes.damouna@gmail.com>
2024-01-09 20:24:33 +0200

    backend: cleaned appController

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,19 +1,15 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,private authService: AuthService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req:any) {
-    return this.authService.login(req.user);
-  }
+
+ 
 }
