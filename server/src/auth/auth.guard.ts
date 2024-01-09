020efa2ebdc09287fde10744b73fd77d1cdea9fe
younes_damouna <younes.damouna@gmail.com>
2024-01-09 23:17:46 +0200

    backend: implemented canActivate in authGuard

--- server/src/auth/auth.guard.ts
+++ server/src/auth/auth.guard.ts
@@ -13,7 +13,24 @@ import { jwtConstants } from 'src/strategy/constants';
     constructor(private jwtService: JwtService) {}
   
     async canActivate(context: ExecutionContext): Promise<boolean> {
-    
+      const request = context.switchToHttp().getRequest();
+      const token = this.extractTokenFromHeader(request);
+      if (!token) {
+        throw new UnauthorizedException();
+      }
+      try {
+        const payload = await this.jwtService.verifyAsync(
+          token,
+          {
+            secret: jwtConstants.secret
+          }
+        );
+        // 💡 We're assigning the payload to the request object here
+        // so that we can access it in our route handlers
+        request['user'] = payload;
+      } catch {
+        throw new UnauthorizedException();
+      }
       return true;
     }
   
