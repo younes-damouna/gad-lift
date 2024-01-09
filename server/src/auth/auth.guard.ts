2323a89f6d79ce7c6513bbd06b9986f866c6faaa
younes_damouna <younes.damouna@gmail.com>
2024-01-09 23:17:21 +0200

    backend: implemented extractToken function in authGuard

---
+++ server/src/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  import { JwtService } from '@nestjs/jwt';
+  import { Request } from 'express';
+import { jwtConstants } from 'src/strategy/constants';
+  
+  @Injectable()
+  export class AuthGuard implements CanActivate {
+    constructor(private jwtService: JwtService) {}
+  
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+    
+      return true;
+    }
+  
+    private extractTokenFromHeader(request: Request): string | undefined {
+      const [type, token] = request.headers.authorization?.split(' ') ?? [];
+      return type === 'Bearer' ? token : undefined;
+    }
+  }
