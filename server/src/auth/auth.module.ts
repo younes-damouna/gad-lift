d2623f096cdee49ee2a8bc168b999dd561abbcc3
younes_damouna <younes.damouna@gmail.com>
2024-01-09 13:51:21 +0200

    backend: imported jwt

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/strategy/constants';
 
 @Module({
-  imports:[UsersModule,PassportModule],
+  imports:[UsersModule,PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: {
+        expiresIn: '60d'
+      },
+    }),],
   controllers: [AuthController],
   providers: [AuthService,LocalStrategy]
 })
