a4b9cd4d59e2f2b7872d73ea42f151684d9ddd3f
younes_damouna <younes.damouna@gmail.com>
2024-01-09 20:41:20 +0200

    backend: edited validateUser in authService

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { usersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService:usersService){}
+
+    async validateUser(email:string,password:string){
+        const user=await this.usersService.findOne(email);
+        if(user &&user.password===password){
+            const {password ,...result}=user;
+            return result;
+
+        }
+        return null;
+
+
+    }
+}
