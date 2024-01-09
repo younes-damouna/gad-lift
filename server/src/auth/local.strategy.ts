ec49938e558354214fbbb615d9688e4c89799a11
younes_damouna <younes.damouna@gmail.com>
2024-01-09 12:54:18 +0200

    backend: created localStrategy

--- server/src/auth/local.strategy.ts
+++ server/src/auth/local.strategy.ts
@@ -9,15 +9,7 @@ export class LocalStrategy extends PassportStrategy(Strategy, "local"){
         super();
     }
 
-    async validate(email:string,password:string):Promise<any>{
-        const user=await this.authService.validateUser(email,password);
-        if (!user){
-            throw new UnauthorizedException();
-
-        }
-        return user;
-
-    }
+   
         
 }
 
