09b50f399cfa5bf79bdab21d8208ee529f077527
younes_damouna <younes.damouna@gmail.com>
2024-01-09 12:11:46 +0200

    backend: implemented hashPassword function

--- server/src/users/hash.service.ts
+++ server/src/users/hash.service.ts
@@ -3,6 +3,11 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class HashService{
+async hashPassword(password: string){
+    const saltOrRounds=10;
+    return await bcrypt.hash(password,saltOrRounds);
 
+
+}
 
 }
