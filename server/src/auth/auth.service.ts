967ca62fb2df428190bf3802e807de6f35ec62ff
younes_damouna <younes.damouna@gmail.com>
2024-01-09 12:33:09 +0200

    backend: implemented login

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -7,27 +7,30 @@ export class AuthService {
     constructor(private usersService: usersService) { }
 
     async validateUser(email: string, password: string): Promise<any> {
-        const user: User = await this.usersService.findOne(email);
-        if (user && user.password === password) {
-            const { password, ...result } = user;
-            return result;
-        }
-        return null;
+        
 
     }
 
     async login(email: string, pass: string): Promise<any> {
 
+        // const user: User = await this.usersService.findOne(email);
+        // console.log(user?.password)
+        // if (user?.password !== pass) {
+
+        //     throw new UnauthorizedException();
+
+        // }
+        // const { password, ...result } = user;
+        // // TODO: Generate a JWT and return it here
+        // // instead of the user object
+        // return user;
+
         const user: User = await this.usersService.findOne(email);
-        console.log(user?.password)
-        if (user?.password !== pass) {
-
-            throw new UnauthorizedException();
-
+        if (user && user.password === pass) {
+            const { password, ...result} = user;
+            console.log(result)
+            return result;
         }
-        const { password, ...result } = user;
-        // TODO: Generate a JWT and return it here
-        // instead of the user object
-        return result;
+        return null;
     }
 }
