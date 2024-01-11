b50912c0a449f53e6bda3d2643d220f9ee13718d
younes_damouna <younes.damouna@gmail.com>
2024-01-11 20:20:25 +0200

    backend: implemented validate function in googleStrategy

--- server/src/strategy/google.strategy.ts
+++ server/src/strategy/google.strategy.ts
@@ -2,22 +2,36 @@ import { Inject, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import passport from "passport";
-import { Strategy } from "passport-google-oauth2";
+import { Strategy, VerifyCallback } from "passport-google-oauth2";
 
 import { config } from 'dotenv';
 config();
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy,'google'){
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 
     constructor(
-      
-        
-    ){
+
+
+    ) {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_SECRET,
             callbackURL: process.env.CALLBACK_URL,
             scope: ['email', 'profile'],
-          });
+        });
+    }
+
+
+    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+        const { name, emails, photos } = profile;
+        const user = {
+            email: emails[0].value,
+            firstName: name.givenName,
+            lastName: name.familyName,
+            picture: photos[0].value,
+            accessToken
+        }
+        done(null, user);
+
     }
 }
