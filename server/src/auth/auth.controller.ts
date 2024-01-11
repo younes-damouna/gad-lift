68eb6cf5b5cfda6baab1a41aa089f59a151d6a60
younes_damouna <younes.damouna@gmail.com>
2024-01-11 16:25:48 +0200

    backend: edited auth routes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards, Get, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { CreateUserDto } from 'src/users/dto/CreateUser.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +11,11 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+  @HttpCode(HttpStatus.OK)
+  @Post('register')
+  register(@Body() createUserDto:CreateUserDto) {
+    return this.authService.register(createUserDto);
   }
 
   @UseGuards(AuthGuard)
