c154a3fa5ed45bd256c211b3fd201ffde9da0005
younes_damouna <younes.damouna@gmail.com>
2024-01-11 09:35:37 +0200

    backend: added props to familySchema]

--- server/src/schemas/family.schema.ts
+++ server/src/schemas/family.schema.ts
@@ -1,5 +1,24 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { User } from "./user.schema";
 
 
-@Schema({timestamps:true})
-export class Family{}
+@Schema({ timestamps: true })
+export class Family {
+    @Prop({
+        required: true,
+    })
+    code: string;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+    parent: User;
+
+    @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: 'User' })
+    requests?: User[];
+
+    @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: 'User' })
+    members: { status: string, user: User }[];
+
+
+
+}
