3bbf89e5b385232c2e2d249ef764c13731bdab1b
younes_damouna <younes.damouna@gmail.com>
2024-01-11 10:51:44 +0200

    backend: implemented constructor in familiesController

--- server/src/families/families.controller.ts
+++ server/src/families/families.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { FamiliesService } from './families.service';
 
 @Controller('families')
-export class FamiliesController {}
+export class FamiliesController {
+    constructor(private familyService:FamiliesService){}
+
+}
