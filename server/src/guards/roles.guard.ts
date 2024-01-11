d1dca5f5cd7728c69b45c12bb4723d518ac450d8
younes_damouna <younes.damouna@gmail.com>
2024-01-11 19:19:49 +0200

    backend: deleted comments in rolesGuard

--- server/src/guards/roles.guard.ts
+++ server/src/guards/roles.guard.ts
@@ -5,21 +5,16 @@ import { Role } from 'src/enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+    constructor(private reflector: Reflector) { }
 
-  canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    // if (!requiredRoles) {
-     
-
-    //   return true;
-    // }
-    const { user } = context.switchToHttp().getRequest();
-    // console.log(user)
-   
-    return requiredRoles.some((role) => user.user.user_type?.includes(role));
-  }
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+       
+        const { user } = context.switchToHttp().getRequest();
+      
+        return requiredRoles.some((role) => user.user.user_type?.includes(role));
+    }
 }
