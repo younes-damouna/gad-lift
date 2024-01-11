d0624f972ed960d4d1a88f1caa3e4f659a04a6cf
younes_damouna <younes.damouna@gmail.com>
2024-01-11 16:28:59 +0200

    backend: implemented register logic in authService

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,32 +1,37 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Body, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/entities/user.entity';
 import { HashService } from 'src/common/hash.service';
+import { CreateUserDto } from 'src/users/dto/CreateUser.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService,private jwtService: JwtService,private hashService:HashService) {}
+  constructor(private usersService: UsersService, private jwtService: JwtService, private hashService: HashService) { }
   async validateUser(email: string, password: string): Promise<any> {
-        
+
     const user: User = await this.usersService.findOne(email);
-    if (user && (await this.hashService.comparePassword(password , user.password))) {
-       
-        return user;
+    if (user && (await this.hashService.comparePassword(password, user.password))) {
+
+      return user;
     }
     throw new UnauthorizedException();
-}
+  }
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto)
+  }
   async signIn(email: string, pass: string): Promise<any> {
-    const user = await this.validateUser(email,pass);
-   
-    
- 
+    const user = await this.validateUser(email, pass);
 
-    const payload = {  user: user};
+
+
+
+    const payload = { user: user };
     return {
-        user:user,
+      user: user,
       access_token: await this.jwtService.signAsync(payload),
     };
-  
+
   }
+
 }
