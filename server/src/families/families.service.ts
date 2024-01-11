cb0368ce645cc43e663aa2edb13a9dd676b4c151
younes_damouna <younes.damouna@gmail.com>
2024-01-11 12:33:01 +0200

    backend: implemented findFamily

--- server/src/families/families.service.ts
+++ server/src/families/families.service.ts
@@ -8,32 +8,43 @@ import { createFamilyDto } from './dto/CreateFamily.dto';
 export class FamiliesService {
     constructor(@InjectModel(Family.name) private FamilyModel: Model<Family>) {
 
-      
+
     }
-    async createFamily(createFamilyDto:createFamilyDto,@Request() req){
-       const user=await this.checkUserHaveFamily(req.user.user._id);
-       if (user) {
-        console.log(user);
-        throw new BadRequestException({message:"Already in a family"});
-    }
-        const family= new this.FamilyModel(createFamilyDto);
-        family.code=this.createRandomFamilyCode();
-        family.parent=req.user.user._id;
-         const createdFamily=(await family.save()).populate('parent')
+    async createFamily(createFamilyDto: createFamilyDto, @Request() req) {
+        const user = await this.checkUserHaveFamily(req.user.user._id);
+        
+        if (user) {
+            console.log(user);
+            throw new BadRequestException({ message: "Already in a family" });
+        }
+        const family = new this.FamilyModel(createFamilyDto);
+        family.code = this.createRandomFamilyCode();
+        family.parent = req.user.user._id;
+        const createdFamily = (await family.save()).populate('parent')
         // console.log(req.user.user._id)
         // const family 
         return createdFamily;
 
     }
-    
-    createRandomFamilyCode() :string{
-        
-        return parseInt((Math.random()*1000000).toString()).toString();
+
+    createRandomFamilyCode(): string {
+
+        return parseInt((Math.random() * 1000000).toString()).toString();
 
     }
-    async checkUserHaveFamily(id:mongoose.Schema.Types.ObjectId){
-        const user =await this.FamilyModel.findOne({parent:id});
+    async checkUserHaveFamily(id: mongoose.Schema.Types.ObjectId) {
+        const user = await this.FamilyModel.findOne({ parent: id });
         return user;
+
+    }
+    // async requestTojoinFamily(id:mongoose.Schema.Types.ObjectId,code:string){
+    //     // search for the family based on its code
+
+    // }
+
+    async findFamily(code:string){
+        const family =await this.FamilyModel.findOne({code});
+        return family;
 
     }
 }
