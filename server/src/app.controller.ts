04bcb5385d46f32450353763f3aa2af4f4bedbf9
younes_damouna <younes.damouna@gmail.com>
2024-01-11 20:24:46 +0200

    backend: added google routes to appController

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 
-@Controller()
+@Controller('google')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {}
   @Get()
   getHello(): string {
     return this.appService.getHello();
