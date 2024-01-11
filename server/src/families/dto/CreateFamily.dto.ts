e0ce8d0125f28e693e27164c48843c93eceffe46
younes_damouna <younes.damouna@gmail.com>
2024-01-11 10:19:56 +0200

    backend: added validateNested to parent prop in createFamilyDto

--- server/src/families/dto/CreateFamily.dto.ts
+++ server/src/families/dto/CreateFamily.dto.ts
@@ -1,17 +1,21 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsOptional, IsString, ValidateNested } from "class-validator";
 import { User } from "src/entities/user.entity";
+import { CreateUserDto } from "src/users/dto/CreateUser.dto";
 
 export class createFamilyDto{
     @IsString()
     code: string;
 
     @IsNotEmpty()
-    parent: User;
+    @ValidateNested()
+    @Type(()=>CreateUserDto)
+    parent: CreateUserDto;
 
     @IsOptional()
     requests: User[];
-    @IsOptional()
 
+    @IsOptional()
     members: {
         status: string,
         user: User
