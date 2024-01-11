a3c5fe0e7129806bb8fdc56f44b078da59208cc4
younes_damouna <younes.damouna@gmail.com>
2024-01-11 10:13:12 +0200

    backend: created the constructor i familiesService

--- server/src/families/families.service.ts
+++ server/src/families/families.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Family } from 'src/schemas/family.schema';
 
 @Injectable()
-export class FamiliesService {}
+export class FamiliesService {
+    constructor(@InjectModel(Family.name) private FamilyModel: Model<Family>) {
+
+      
+    }
+    
+}
