053c4931a976d229bbc31f31c7f27324946620a8
younes_damouna <younes.damouna@gmail.com>
2024-01-13 13:10:41 +0200

    frontend: formated cameraView widget

--- mobile_app/lib/screens/dashboard.screen.dart
+++ mobile_app/lib/screens/dashboard.screen.dart
@@ -51,7 +51,7 @@ class DashboardScreen extends StatelessWidget {
               ),
             ),
           ),
-         
+        const CameraView(),
         ],
       ),
     );
--- mobile_app/lib/widgets/camera_view.widget.dart
+++ mobile_app/lib/widgets/camera_view.widget.dart
@@ -10,6 +10,26 @@ class CameraView extends StatefulWidget {
 class _CameraViewState extends State<CameraView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: const BoxDecoration(
+         color:  Color(0x63FFFFFF),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      // height: 300,
+      margin: const EdgeInsets.all(20),
+      width: double.infinity,
+      child: Container(
+        color: const Color.fromARGB(22,22,22, 22),
+        height: 200,
+        child: const Text('data'),
+      ),
+    );
   }
 }
