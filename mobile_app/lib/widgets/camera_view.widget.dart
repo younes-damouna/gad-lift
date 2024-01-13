eecb51b3070c6bbae0ea097947da44342f0adc63
younes_damouna <younes.damouna@gmail.com>
2024-01-13 12:43:48 +0200

    frontend: created cameraView widget

---
+++ mobile_app/lib/widgets/camera_view.widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraView extends StatefulWidget {
+  const CameraView({super.key});
+
+  @override
+  State<CameraView> createState() => _CameraViewState();
+}
+
+class _CameraViewState extends State<CameraView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
