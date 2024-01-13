65cbe618499d44e1515feed5f445176208a56809
younes_damouna <younes.damouna@gmail.com>
2024-01-13 19:37:07 +0200

    frontend: imported images folder

--- mobile_app/lib/screens/welcome.screen.dart
+++ mobile_app/lib/screens/welcome.screen.dart
@@ -7,10 +7,11 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
         body: Center(
-          child: Image(
-        image: NetworkImage(
-            'https://raw.githubusercontent.com/younes-damouna/younes-damouna/main/github-banner.png'),
-      ),
+      child: Image(image: AssetImage('animatedLogo.png'))
+      // Image(
+      //   image: NetworkImage(
+      //       'https://raw.githubusercontent.com/younes-damouna/younes-damouna/main/github-banner.png'),
+      // ),
     ));
   }
 }
--- mobile_app/lib/widgets/common/button_title.widget.dart
+++ mobile_app/lib/widgets/common/button_title.widget.dart
@@ -4,15 +4,15 @@ import 'package:google_fonts/google_fonts.dart';
 class ButtonTitle extends StatelessWidget {
   final String title;
   final double size;
-  const ButtonTitle({super.key,required this.title,required this.size});
+  const ButtonTitle({super.key, required this.title, required this.size});
 
   @override
   Widget build(BuildContext context) {
-    return  Text(
+    return Text(
       title,
       style: GoogleFonts.zenDots(
         fontSize: size,
-         color: Colors.black,
+        color: Colors.black,
         // fontWeight: FontWeight.w900,
       ),
     );
