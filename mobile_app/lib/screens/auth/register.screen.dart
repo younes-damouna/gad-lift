4cdf6fdc7dbd70a3f1354279ded61be2a005ec86
younes_damouna <younes.damouna@gmail.com>
2024-01-13 22:05:38 +0200

    frontend: implemented registerScreen widget

---
+++ mobile_app/lib/screens/auth/register.screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor:  Color(0xFFF3F1F1),
+      body: Center(child: Text('loginScreen')),
+    );
+  }
+}
