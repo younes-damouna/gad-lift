fe71fd254aed627220fafeb8ea3c54bd47c68ada
younes_damouna <younes.damouna@gmail.com>
2024-01-14 03:52:37 +0200

    frontend: implemented profileScreen widget

---
+++ mobile_app/lib/screens/profile.screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/widgets/app_bar.widget.dart';
+import 'package:mobile_app/widgets/camera_view.widget.dart';
+import 'package:mobile_app/widgets/common/section_title.dart';
+import 'package:mobile_app/widgets/common/secondary_button.widget.dart';
+import 'package:mobile_app/widgets/navigation.widget.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: MyAppBar(title: 'Profile'),
+      backgroundColor: Color(0xFFF3F1F1),
+      body: Center(
+        child: Text('Profile'),
+      ),
+      bottomNavigationBar: Navigation(),
+    );
+  }
+}
