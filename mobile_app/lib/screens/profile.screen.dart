44e467b6b6cdf27b7bea0cc037a30ebaa765a161
younes_damouna <younes.damouna@gmail.com>
2024-01-14 04:12:40 +0200

    frontend: formatted profileScreen widget code

--- mobile_app/lib/screens/profile.screen.dart
+++ mobile_app/lib/screens/profile.screen.dart
@@ -10,50 +10,48 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: const MyAppBar(title: 'Profile'),
       backgroundColor: const Color(0xFFF3F1F1),
       body: Column(
-      children: [
-        Container(
-          height: 500,
-          decoration: const BoxDecoration(
-            // color: Color.fromARGB(98, 255, 255, 255),
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            color: Color(0xf1f1f1f1),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 5,
-                offset: Offset(0, 0),
-              ),
-            ],
-          ),
-          // height: 300,
-          margin: const EdgeInsets.all(20),
-          width: double.infinity,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-
-            // color: const Color.fromARGB(22, 22, 22, 22),
-            //  color: const Color(0x63FFFFFF),
-            height: 200,
-            child: const Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SectionTitle(title: 'Personal Information', size: 18),
-                  
-                  ],
+        children: [
+          Container(
+            height: 500,
+            decoration: const BoxDecoration(
+              // color: Color.fromARGB(98, 255, 255, 255),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Color(0xf1f1f1f1),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5,
+                  offset: Offset(0, 0),
                 ),
-              
               ],
             ),
+            // height: 300,
+            margin: const EdgeInsets.all(20),
+            width: double.infinity,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+
+              // color: const Color.fromARGB(22, 22, 22, 22),
+              //  color: const Color(0x63FFFFFF),
+              height: 200,
+              child: const Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SectionTitle(title: 'Personal Information', size: 18),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ],
-    ),
+        ],
+      ),
       bottomNavigationBar: const Navigation(),
     );
   }
