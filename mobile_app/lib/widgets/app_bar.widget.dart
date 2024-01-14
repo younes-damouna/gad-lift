bbe4bc0424ea10b88f8de0a3c8b57af3446e9880
younes_damouna <younes.damouna@gmail.com>
2024-01-14 03:23:34 +0200

    frontend: fixed appBar widget

--- mobile_app/lib/widgets/app_bar.widget.dart
+++ mobile_app/lib/widgets/app_bar.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/main.dart';
 // import 'package:mobile_app/main.dart';
 class MyAppBar extends StatelessWidget   {
   const MyAppBar({
@@ -7,24 +8,63 @@ class MyAppBar extends StatelessWidget   {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      // excludeHeaderSemantics: true,
-      // title: screenTitle(title: 'GadLift', size: 50),
-      foregroundColor: Colors.white,
-      backgroundColor: const Color(0xFF2FE2EE),
-      bottomOpacity: 0.5,
-      toolbarHeight: 160,
-      elevation: 5.0,
-      leadingWidth: 40,
+    return PreferredSize(
+          // scrolledUnderElevation: 1,
+          
+          // excludeHeaderSemantics: true,
+          title: Column(
+            children: [
+              screenTitle(title: 'GadLift', size: 50),
+            screenTitle(title: 'Lift Your Gadgets With Us', size: 10),],
+          ),
+          bottom:  const PreferredSize(
+            preferredSize: Size(100, 100),
+            child: Text(''),
+            // child: Text(
+            //   'hello',
+            // )
+          ),
+          // foregroundColor: Colors.white,
+          backgroundColor: const Color(0xFF2FE2EE),
+          bottomOpacity: 0.5,
+          // toolbarHeight: 140,
+          
+          toolbarHeight: 80,
+          elevation: 5.0,
+          leadingWidth: 40,
+          titleTextStyle:  const TextStyle(
+            color: Colors.white,
+          ),
+
+          // leadingWidth: 100,
+          shadowColor:  const Color(0xFF2FE2EE),
+          // shape: const CircleBorder(
+          //   side: BorderSide(width: 5, color: Colors.purple),
+          // ),
+          shape: const RoundedRectangleBorder(
+            
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(1000))),
+        );
     
-      // leadingWidth: 100,
-      shadowColor: const Color(0xFF2FE2EE),
-      // shape: const CircleBorder(
-      //   side: BorderSide(width: 5, color: Colors.purple),
-      // ),
-      shape: const RoundedRectangleBorder(
-          borderRadius:
-              BorderRadius.vertical(bottom: Radius.circular(1000))),
-    );
+    // AppBar(
+    //   // excludeHeaderSemantics: true,
+    //   // title: screenTitle(title: 'GadLift', size: 50),
+    //   foregroundColor: Colors.white,
+    //   backgroundColor: const Color(0xFF2FE2EE),
+    //   bottomOpacity: 0.5,
+    //   toolbarHeight: 160,
+    //   elevation: 5.0,
+    //   leadingWidth: 40,
+    
+    //   // leadingWidth: 100,
+    //   shadowColor: const Color(0xFF2FE2EE),
+    //   // shape: const CircleBorder(
+    //   //   side: BorderSide(width: 5, color: Colors.purple),
+    //   // ),
+    //   shape: const RoundedRectangleBorder(
+    //       borderRadius:
+    //           BorderRadius.vertical(bottom: Radius.circular(1000))),
+    // );
   }
 }
