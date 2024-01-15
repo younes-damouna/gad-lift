59612ca856fe74bb7b3aa81f732cee5ab6d83fbe
younes_damouna <younes.damouna@gmail.com>
2024-01-16 01:29:10 +0200

    frontend: implemented register function in authService

--- mobile_app/lib/helpers/api/services/auth.service.dart
+++ mobile_app/lib/helpers/api/services/auth.service.dart
@@ -1,21 +1,34 @@
-
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:mobile_app/helpers/config/base_dio.dart';
 import 'package:mobile_app/helpers/constants/api_constants.dart';
 
-abstract class AuthService{
-static Future login(String email,String password) async{
-try {
-    final response= await dio.post(ApiRoutes.login,data: {
-    "email":email,
-    "password":password
-  });
-  log(response as String);
-} on DioException catch(e) {
-  log(e as String);
-}
+abstract class AuthService {
+  static Future login(String email, String password) async {
+    try {
+      final response = await dio.post(ApiRoutes.login, data: {
+        "email": email,
+        "password": password,
+      });
+      log(response as String);
+    } on DioException catch (e) {
+      log(e as String);
+    }
+  }
 
-}
+  static Future register(
+      String firstName, String lastName, String email, String password) async {
+    try {
+      final response = await dio.post(ApiRoutes.login, data: {
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email,
+        "password": password,
+      });
+      log(response as String);
+    } on DioException catch (e) {
+      log(e as String);
+    }
+  }
 }
