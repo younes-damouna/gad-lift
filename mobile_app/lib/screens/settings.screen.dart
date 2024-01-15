ea7ab726e3b3369aa86d55284606fb11bfc394b4
younes_damouna <younes.damouna@gmail.com>
2024-01-15 14:15:03 +0200

    frontend: implemented settingsScreen container

--- mobile_app/lib/screens/settings.screen.dart
+++ mobile_app/lib/screens/settings.screen.dart
@@ -1,44 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/widgets/app_bar.widget.dart';
+import 'package:mobile_app/widgets/common/section_title.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:  const Color(0xFFF3F1F1),
-      body: Container(
-        margin: const EdgeInsets.all(20),
-        height: 50,
-        // color: const Color(0xFF2FE2EE),
-        decoration: BoxDecoration(
-            // image: DecorationImage(
-            //   image: NetworkImage(
-            //       'https://media.geeksforgeeks.org/wp-content/cdn-uploads/logo.png'),
-            // ),
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            border: Border.all(
-                color: const Color(0xFF2FE2EE),
-                strokeAlign: BorderSide.strokeAlignInside,
-                width: 2,
-                style: BorderStyle.solid),
-            color: Colors.white,
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 5,
-                offset: Offset(0, 0),
-              )
-            ]),
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text('Device Status'), Icon(Icons.circle,color: Colors.green,)],
-            
+     return Scaffold(
+      appBar: const MyAppBar(title: 'Settings'),
+      backgroundColor: const Color(0xFFF3F1F1),
+      body: Column(
+        children: [
+          Container(
+            height: 500,
+            decoration: const BoxDecoration(
+              // color: Color.fromARGB(98, 255, 255, 255),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Color(0xf1f1f1f1),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+            // height: 300,
+            margin: const EdgeInsets.all(20),
+            width: double.infinity,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+
+              // color: const Color.fromARGB(22, 22, 22, 22),
+              //  color: const Color(0x63FFFFFF),
+              height: 200,
+              child: const Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SectionTitle(title: 'Requests', size: 18),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
+      // bottomNavigationBar: const Navigation(),
     );
   }
 }
