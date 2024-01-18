522e6a423b16a5ec0256f239967147aa8524d6a3
younes_damouna <younes.damouna@gmail.com>
2024-01-18 12:47:39 +0200

    frontend: implemented get profile

--- mobile_app/lib/helpers/providers/profile_provider.dart
+++ mobile_app/lib/helpers/providers/profile_provider.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/helpers/models/user.model.dart';
 
-class ProfileProvider with ChangeNotifier{
-  final String id;
-  final String email;
-  final String firstName;
-  final String lastName;
-  
-  ProfileProvider({
-    required this.id,
-    required this.email,
-    required this.firstName,
-    required this.lastName
+class ProfileProvider with ChangeNotifier {
+   String id;
+   String email;
+   String firstName;
+   String lastName;
 
-  });
+  ProfileProvider(
+      {required this.id,
+      required this.email,
+      required this.firstName,
+      required this.lastName,});
+
+      void getProfile(User user){
+        id=user.id;
+        email=user.email;
+        firstName=user.firstName;
+        lastName=user.lastName;
+
+        notifyListeners();
+        
+      }
 }
