7278ba0fffcd3ef9b0f2f0cd848f2edbd08f1358
younes_damouna <younes.damouna@gmail.com>
2024-01-21 17:34:51 +0200

    arduino: connected the motor

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,35 +1,70 @@
 #include <Arduino.h>
+#include "./wifi/wifi.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define RED_LED 5
+int ENA = 4;
 
+int IN1 = 0;
+
+int IN2 = 2;
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  //  Begin Serial
+     pinMode(RED_LED,OUTPUT);
+     // set all the motor control pins to outputs
 
-void loop() {
+pinMode(ENA, OUTPUT);
+pinMode(IN1, OUTPUT);
+pinMode(IN2, OUTPUT);
+
+  Serial.begin(9600);
+  // //Connect to wifi
+  setupWifi();
+
+  //Check if wifi is connected
+  // if (WiFi.status() == WL_CONNECTED) {
+  //    delay(1000);
+  //   Serial.println("Error in WiFi connection");
+  //     Serial.println(1);
+  //       digitalWrite(RED_LED,HIGH);
+
+    
+  // } else {
+  //        delay(1000);
+  // Serial.println(2);
+  //   Serial.println("Error in WiFi connection");
+  // }
+}
+void testOne() {
+
+// turn on motor
+delay(2000); // now change motor directions
+
+analogWrite(ENA, 150); // set speed to 200 out of possible range 0~255
+digitalWrite(IN1, HIGH);
+digitalWrite(IN2, LOW);
+
+delay(2000); // now change motor directions
+
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, HIGH);
+
+delay(2000); // now turn off motors
+
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, LOW);
+delay(2000); // now turn off motors
+
+}
+void loop() {  
+  delay(1000);
+  digitalWrite(RED_LED,HIGH);
+
+delay(1000);
+ digitalWrite(RED_LED,LOW);
+// delay(1000);
+// testOne();   
+// 	delay(1000); 
+
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-// 
-// {
-//     "configurations": [
-//         {
-//             "name": "Win32",
-//             "includePath": [
-//                 "${workspaceFolder}/**"
-//             ],
-//             "defines": [
-//                 "_DEBUG",
-//                 "UNICODE",
-//                 "_UNICODE"
-//             ]
-//         }
-//     ],
-//     "version": 4
-// }
