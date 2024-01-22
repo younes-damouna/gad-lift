2d7f9e3698631aa73c6f8017d92a2e788e4c6bbf
younes_damouna <younes.damouna@gmail.com>
2024-01-23 00:42:32 +0200

    web-frontend: implemented form group

--- web_frontend/src/components/FormGroup/index.jsx
+++ web_frontend/src/components/FormGroup/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './index.css'
-const FormGroup = () => {
+const FormGroup = ({ label, placeholder, className, type,required,onChange,name,value }) => {
   return (
-    <div>FormGroup</div>
-  )
-}
-
+    <div className="form-group d-flex column">
+      <label htmlFor="email text-white ">{label}</label>
+      <input
+        className={className}
+        placeholder={placeholder}
+        type={type}
+        id={label}
+        name={name}
+        required={required}
+        value={value}
+        onChange={(e)=>onChange(name,e.target.value)}
+      />
+    </div>
+  );
+  }
 export default FormGroup
