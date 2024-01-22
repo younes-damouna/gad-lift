b8559705dc51942841bea4410e5b59469539a481
younes_damouna <younes.damouna@gmail.com>
2024-01-22 23:47:54 +0200

    web-frontend: created landing page route

--- web_frontend/src/App.jsx
+++ web_frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import {BrowserRouter, Routes} from 'react-router-dom';
+import LandingPage from './pages/Landing';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>{/* <Route element={<HomePage />} path="/" /> */}</Routes>
+        <Routes>
+          <Route element={<LandingPage />} path="/" />
+          </Routes>
       </BrowserRouter>
     </div>
   );
