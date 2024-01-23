9ba295be57be8d65b1bacd85e1a3d7b923003901
younes_damouna <younes.damouna@gmail.com>
2024-01-23 23:35:01 +0200

    arduino: created wifi example file

---
+++ Arduino/src/wifi/wifi.example.cpp
@@ -0,0 +1,23 @@
+#include "wifi.h"
+
+// Wifi credentials
+const char *ssidd = "SSID of your network";
+const char *passwordd = "your wifi password";
+
+// Setup wifi
+void setupWifi()
+{
+  // Connect to wifi
+  WiFi.begin(ssidd, passwordd);
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    // Wait 1 second
+    delay(1000);
+    Serial.println("*");
+  }
+  delay(1000);
+
+ 
+};
