9055d9fcd1f349e742b0ed0329f37657317999b5
younes_damouna <younes.damouna@gmail.com>
2024-01-23 17:39:45 +0200

    backend: implemented device schema

---
+++ server/src/schemas/device.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { Family } from "./family.schema";
+import { Timestamp } from "rxjs";
+
+
+export type DeviceDocument = Device & Document;
+@Schema({ timestamps: true })
+export class Device {
+    @Prop({
+        required: true,
+
+    })
+    mac_address: string;
+    @Prop({
+        required: true,
+
+    })
+    ip_address: string;
+
+    @Prop({})
+    building_name: string;
+
+    // @Prop()
+    // logs:[
+        
+    // ];
+
+}
+export const UserSchema = SchemaFactory.createForClass(Device);
