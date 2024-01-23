1f2f8dc43d58b29beed1b3c2727367604374c238
younes_damouna <younes.damouna@gmail.com>
2024-01-23 22:38:53 +0200

    backend: implemented turn motor up function

--- server/src/devices/devices.service.ts
+++ server/src/devices/devices.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { HttpService } from "@nestjs/axios";
+import axios from 'axios';
 
 @Injectable()
-export class DevicesService {}
+export class DevicesService {
+    constructor(private httpService:HttpService){}
+
+    async turnMotorUp():Promise<any> {
+            try {
+                console.log('try')
+               const response= await axios.get('http://192.168.0.100/motor/up');
+              
+               return {"message":response.data,
+            "statusCode":200};
+            } catch (error) {
+                console.log(error)
+              // Handle error
+              console.error('Error turning on LED on NodeMCU:', error.message);
+            // return  HttpException('wrong',400);
+            }
+            
+            console.log('called')
+      }
+}
