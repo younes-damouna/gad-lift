8d5af531dc212c271ff8058b5bcb599c923e09e9
younes_damouna <younes.damouna@gmail.com>
2024-01-24 04:08:39 +0200

    frontend: implemented initNotifications function

--- mobile_app/lib/helpers/api/services/firebase.service.dart
+++ mobile_app/lib/helpers/api/services/firebase.service.dart
@@ -1,3 +1,33 @@
+import 'dart:developer';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 class FireBaseService{
-  
+
+  // create an instance of Firebase messaging
+  final _firebaseMessaging=FirebaseMessaging.instance;
+
+
+  //initialize notifications
+  Future<void> initNotifications()async{
+    // request permission from user
+    await _firebaseMessaging.requestPermission();
+
+    // fetch firebase cloud messaging token (FCM) for this device
+    final fcmToken=await _firebaseMessaging.getToken();
+
+    // print token(send to server)
+    log('fcmToken $fcmToken');
+
+
+
+
+       
+
+  }
+
+  //handle received messages
+
+
+  //initialize foreground and background settings
 }
