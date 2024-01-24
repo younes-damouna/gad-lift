401fa7d772390866f2654226f2535d4df04f95ac
younes_damouna <younes.damouna@gmail.com>
2024-01-24 23:50:44 +0200

    backend: implemented file controller

--- server/src/file.controller.ts
+++ server/src/file.controller.ts
@@ -1,1 +1,18 @@
- 
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { join } from 'path';
+import { existsSync } from 'fs';
+
+@Controller('')
+export class FileController {
+  @Get('uploads/:filename')
+  serveFile(@Param('filename') filename: string, @Res() res) {
+    const path = join(__dirname, '..', 'uploads', filename);
+    console.log(path)
+
+    if (existsSync(path)) {
+      return res.sendFile(path);
+    } else {
+      return res.status(404).send('File not found');
+    }
+  }
+}
