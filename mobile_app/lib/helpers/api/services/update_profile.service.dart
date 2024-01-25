1f8700e0772fc8eaa0efa4906eed0b44036cc757
younes_damouna <younes.damouna@gmail.com>
2024-01-25 03:04:40 +0200

    frontend: cleaned update profile service

--- mobile_app/lib/helpers/api/services/update_profile.service.dart
+++ mobile_app/lib/helpers/api/services/update_profile.service.dart
@@ -11,12 +11,12 @@ abstract class UserService {
   static Future updateUser(String? firstName, String? lastName, String? email,
       String id, XFile? pickedFile) async {
     try {
-      final now = DateTime.now().millisecondsSinceEpoch;
       FormData formData = FormData();
       if (pickedFile != null) {
         formData = FormData.fromMap({
           "profile_img": await MultipartFile.fromFile(pickedFile.path,
-              filename: pickedFile.name.toString().replaceAll(' ', '').toLowerCase()),
+              filename:
+                  pickedFile.name.toString().replaceAll(' ', '').toLowerCase()),
           "first_name": firstName,
           "last_name": lastName,
           "email": email,
@@ -39,35 +39,11 @@ abstract class UserService {
       final response = await dio.post(
         '${ApiRoutes.profile}/$id',
         data: formData,
-        // queryParameters: {"id":id}
       );
-      log(response.statusCode as String);
-      log('response : ${response.data}');
+     
       return response.data;
     } on DioException catch (e) {
       return e.response;
     }
   }
-
-//   static Future changeImage(XFile? pickedFile, String id) async {
-//     if (pickedFile != null) {
-//       FormData formData = FormData.fromMap({
-//         "profile_img": await MultipartFile.fromFile(pickedFile.path,
-//             filename: now.toString()),
-//       });
-
-//       final response =
-//           await dio.post('${ApiRoutes.profile}/$id', data: formData);
-
-//       if (response.statusCode == 200) {
-//         log('200');
-
-//         // return '${response.statusMessage}';
-//       } else {
-//         log('response');
-
-//         // return 'Image upload failed!';
-//       }
-//     }
-//   }
 }
