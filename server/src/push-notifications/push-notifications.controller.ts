076db3cc4c3fb1f08d35e4547b5a7e0137cfccbe
younes_damouna <younes.damouna@gmail.com>
2024-01-25 21:10:55 +0200

    backend: fixed import in  push notifications controller

--- server/src/push-notifications/push-notifications.controller.ts
+++ server/src/push-notifications/push-notifications.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { PushNotificationsService } from './push-notifications.service';
+import { CreatePushNotificationDto } from './dto/createPushNotifications.dto';
 
 @Controller('push-notifications')
 export class PushNotificationsController {
     constructor(private readonly pushNotificationsService:PushNotificationsService){}
 
     @Post()
-    async send(@Body() pushNotificationDto:PushNotificationDto){
+    async send(@Body() createPushNotificationDto:CreatePushNotificationDto){
         console.log(`method called ${this.send.name}`)
-        return await this.pushNotificationsService.send(pushNotificationsDto);
+        return await this.pushNotificationsService.send(createPushNotificationDto);
 
     }
 }
