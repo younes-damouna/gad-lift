c4fd5f49b551cefd960163a7dd3749b461ac628a
younes_damouna <younes.damouna@gmail.com>
2024-01-25 21:06:40 +0200

    backend: implemented push notifications controller

--- server/src/push-notifications/push-notifications.controller.ts
+++ server/src/push-notifications/push-notifications.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PushNotificationsService } from './push-notifications.service';
 
 @Controller('push-notifications')
-export class PushNotificationsController {}
+export class PushNotificationsController {
+    constructor(private readonly pushNotificationsService:PushNotificationsService){}
+
+    @Post()
+    async send(@Body() pushNotificationDto:PushNotificationDto){
+        console.log(`method called ${this.send.name}`)
+        return await this.pushNotificationsService.send(pushNotificationsDto);
+
+    }
+}
--- server/src/push-notifications/push-notifications.module.ts
+++ server/src/push-notifications/push-notifications.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PushNotificationsController } from './push-notifications.controller';
 import { PushNotificationsService } from './push-notifications.service';
-
+ import * as admin from 'firebase-admin';
 @Module({
   controllers: [PushNotificationsController],
   providers: [PushNotificationsService]
