41f7d7bfe009e8527c3c32a97af79e222ae7dfa1
younes_damouna <younes.damouna@gmail.com>
2024-01-25 21:01:15 +0200

    backend: created push notifications service

--- server/src/push-notifications/push-notifications.module.ts
+++ server/src/push-notifications/push-notifications.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PushNotificationsController } from './push-notifications.controller';
+import { PushNotificationsService } from './push-notifications.service';
 
 @Module({
-  controllers: [PushNotificationsController]
+  controllers: [PushNotificationsController],
+  providers: [PushNotificationsService]
 })
 export class PushNotificationsModule {}
