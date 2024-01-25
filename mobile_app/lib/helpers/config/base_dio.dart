115811e43da98b9439ea304d47d77d3ba008aee5
younes_damouna <younes.damouna@gmail.com>
2024-01-25 03:03:21 +0200

    frontend: cleaned base dio

--- mobile_app/lib/helpers/config/base_dio.dart
+++ mobile_app/lib/helpers/config/base_dio.dart
@@ -1,14 +1,7 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
 
 final BaseOptions options = BaseOptions(
   baseUrl: "http://192.168.0.107:3000",
-  // baseUrl: "http://192.168.0.105:3000",
-
-  // baseUrl: 'http://192.168.44.152:3000',
   contentType: Headers.jsonContentType,
-  
- 
 );
 final Dio dio = Dio(options);
