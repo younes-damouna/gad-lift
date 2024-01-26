9b0b2f4d5d65e25e3e28a5dab9d98a7da80d84c3
younes_damouna <younes.damouna@gmail.com>
2024-01-26 15:35:41 +0200

    frontend: created loading widget

---
+++ mobile_app/lib/widgets/common/loading.widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
