de6780a311c602ddae9ab46361d6c34953c8ae91
younes_damouna <younes.damouna@gmail.com>
2024-01-26 13:26:48 +0200

    frontend: implemented email validation function

--- mobile_app/lib/helpers/validators/input.validator.dart
+++ mobile_app/lib/helpers/validators/input.validator.dart
@@ -16,5 +16,16 @@ class Validator {
     }
   }
 
- 
+  static validateEmail(String value) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = RegExp(pattern);
+    if (value.isEmpty) {
+      return "Email is required";
+    } else if (!regExp.hasMatch(value)) {
+      return "Invalid Email";
+    } else {
+      return null;
+    }
+  }
 }
