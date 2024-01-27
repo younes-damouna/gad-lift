6e74dae24077485012c57596dbf9d711814e9230
younes_damouna <younes.damouna@gmail.com>
2024-01-27 14:08:48 +0200

    frontend: implemented menu in dashboard page

--- web_frontend/src/pages/Dashboard/index.jsx
+++ web_frontend/src/pages/Dashboard/index.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./index.css";
 import BarChart from "../../components/BarChart";
 import BarChartCustom from "../../components/BarChart";
 const DashboardPage = () => {
+  const [activeMainTab,setActiveMainTab]=useState("Dashboard")
+
+  const menuItems=[
+    {
+      title:"Dashboard"
+  },
+    {
+        title:"Users"
+    },
+    {
+        title:"Families"
+    },
+    {
+        title:"Devices"
+    },
+   
+]
   const data = [
     {
       name: "Families",
@@ -23,7 +40,16 @@ const DashboardPage = () => {
   ];
 
   return (
-    <div>
+    <div className="d-flex gap w-100 main">
+      <div className="aside">
+          <div className="menu">
+            {menuItems.map((menuItem,index)=>{return(
+            <div key={index} onClick={()=>{ setActiveMainTab(`${menuItem.title}`)}} className={`menu-item ${activeMainTab===menuItem.title?'active':''}`}>{menuItem.title}</div>
+
+            )})}
+          
+          </div>
+        </div>
       DashboardPage
     <BarChartCustom data={data}/>
     </div>
