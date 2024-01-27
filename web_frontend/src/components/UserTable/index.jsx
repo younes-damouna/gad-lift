fc40ade4b66c1801bcef3bf19c8dc3fb4f4f3ce5
younes_damouna <younes.damouna@gmail.com>
2024-01-28 01:27:14 +0200

    frontend: fixed users table data order

--- web_frontend/src/components/UserTable/index.jsx
+++ web_frontend/src/components/UserTable/index.jsx
@@ -1,56 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { request } from '../../helpers/api';
+import React, { useEffect, useState } from "react";
+import { request } from "../../helpers/api";
 
 const UserTable = () => {
-  const  [users, setusers] = useState([])
+  const [users, setusers] = useState([]);
   useEffect(() => {
     const getUsers = async () => {
       try {
         const response = await request({
           route: "users",
         });
-        setusers(response)
-       
+        setusers(response);
       } catch (error) {
         console.log(error);
       }
     };
     getUsers();
-  
   }, []);
   console.log(users);
   return (
     <div className="table-container">
-    <table>
-      <thead>
-        <tr>
-          <th>Profile Image</th>
+      <table>
+        <thead>
+          <tr>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(users)
+            ? users.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      {user.first_name} {user.last_name}
+                    </td>{" "}
+                    <td>{user.email}</td>
+                    <td>{user.mobile_number}</td>
+                  </tr>
+                );
+              })
+            : ""}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-          <th>Full Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Array.isArray(users)
-          ? users.map((user, index) => {
-              return (
-                <tr key={index}>
-                  <td>
-                    `{user.first_name} {user.last_name}`
-                  </td>
-                  <td>{user.phone_number}</td>
-                  <td>{user.email}</td>
-                </tr>
-              );
-            })
-          : ""}
-      </tbody>
-    </table>
-  </div>
-  )
-}
-
-export default UserTable
+export default UserTable;
