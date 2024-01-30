ee83e77d92148a63a1060d965d8e0da736209d9c
younes_damouna <younes.damouna@gmail.com>
2024-01-30 14:15:28 +0200

    frontend: refactored the source code

--- mobile_app/lib/helpers/validators/input.validator.dart
+++ mobile_app/lib/helpers/validators/input.validator.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
 
 class Validator {
   static validateName(String value) {
-    log('HelloWorld from  $value');
     String pattern = r'(^[a-zA-Z ]*$)';
     RegExp regExp = RegExp(pattern);
     if (value.isEmpty) {
